e1f473206ac6aa075f843bcce7d5632aa27563eb
kwasnakiwi <jakzab199@gmail.com>
2026-01-11 15:31:35 +0100

    repaired images

--- src/utils/GalleryData.jsx
+++ src/utils/GalleryData.jsx
@@ -1,55 +1,65 @@
+import gal1 from './../assets/images/gal1.jpg';
+import gal2 from './../assets/images/gal2.jpg';
+import gal3 from './../assets/images/gal3.jpg';
+import gal4 from './../assets/images/gal4.jpg';
+import gal5 from './../assets/images/gal5.jpg';
+import gal6 from './../assets/images/gal6.jpg';
+import gal7 from './../assets/images/gal7.jpg';
+import gal8 from './../assets/images/gal8.jpg';
+import gal9 from './../assets/images/gal9.jpg';
+
 export const galleryData = [
     {
         id: 1,
-        img: "/src/assets/images/gal1.jpg",
+        img: gal1,
         title: "Katedra Berlińska (Berliner Dom)",
         desc: "Zbliżenie na neorenesansową fasadę i kopułę największej świątyni w mieście. Efekt sepii podkreśla detale architektoniczne.",
     },
     {
         id: 2,
-        img: "/src/assets/images/gal2.jpg",
+        img: gal2,
         title: "Friedrichstraße i Wieża Telewizyjna",
         desc: "Perspektywa miejska z widoczną infrastrukturą tramwajową oraz wieżą Fernsehturm, która jest najwyższym punktem Berlina.",
     },
     {
         id: 3,
-        img: "/src/assets/images/gal3.jpg",
+        img: gal3,
         title: "Brama Brandenburska",
         desc: "Klasycystyczny zabytek z Kwadrygą na szczycie. Zdjęcie przedstawia plac Paryski w pełnym świetle dziennym.",
     },
     {
         id: 4,
-        img: "/src/assets/images/gal4.jpg",
+        img: gal4,
         title: "Kollhoff-Tower (Potsdamer Platz)",
         desc: "Modernistyczny wieżowiec o ceglanej elewacji. Czarno-biały filtr uwydatnia geometryczną formę budynku i rytm okien.",
     },
     {
         id: 5,
-        img: "/src/assets/images/gal5.jpg",
+        img: gal5,
         title: "Gendarmenmarkt i Katedra Francuska",
         desc: "Jeden z najładniejszych placów w Berlinie podczas tzw. blue hour. Ekspozycja skupiona na podświetlonej architekturze katedry.",
     },
     {
         id: 6,
-        img: "/src/assets/images/gal6.jpg",
+        img: gal6,
         title: "Wyspa Muzeów znad Szprewy",
         desc: "Sylwetka Muzeum Bodego i Wieży Telewizyjnej uchwycona we mgle. Minimalistyczny kadr z mocnym kontrastem.",
     },
     {
         id: 7,
-        img: "/src/assets/images/gal7.jpg",
+        img: gal7,
         title: "Panorama przy Alexanderplatz",
         desc: "Wieża telewizyjna na tle zachodzącego słońca. Dobrze widoczna gra świateł na elewacji nowoczesnego biurowca.",
     },
     {
         id: 8,
-        img: "/src/assets/images/gal8.jpg",
+        img: gal8,
         title: "Most Oberbaumbrücke",
         desc: "Charakterystyczna ceglana konstrukcja z wieżyczkami, łącząca dzielnice Friedrichshain i Kreuzberg.",
     },
     {
         id: 9,
-        img: "/src/assets/images/gal9.jpg",
+        img: gal9,
         title: "Rzeźba Molecule Man",
         desc: "Widok z mostu Elsenbrücke na Szprewę. W tle widoczna słynna instalacja artystyczna Jonathana Borofsky'ego.",
     },
